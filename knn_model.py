69e88e6a48df6b9d92e21ab9772e87391ab04302
Vedang Wartikar <vedangwartikar17@gmail.com>
2018-10-05 00:09:26 +0530

    Add files via upload

---
+++ knn_model.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state = 66)
+
+training_confidence = []
+test_confidence = []
+neighbors_range = range(1,11)
+
+from sklearn.neighbors import KNeighborsClassifier
+for n_neighbors in neighbors_range:
+    knn = KNeighborsClassifier(n_neighbors = n_neighbors)
+    knn.fit(X_train, y_train)
+    #Record confidence for train and test sets in array format
+    training_confidence.append(knn.score(X_train, y_train))
+    test_confidence.append(knn.score(X_test, y_test))
+    
+print("Training confidence: {}".format(training_confidence) + "\n")
+print("Test confidence: {}".format(test_confidence) + "\n")
+
+plt.plot(neighbors_range, training_confidence, label = "Training Confidence")
+plt.plot(neighbors_range, test_confidence, label = "Test Confidence")
+plt.ylabel("Confidence")
+plt.xlabel("n_neighbors")
+plt.legend()
